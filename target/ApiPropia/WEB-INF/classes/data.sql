7b66a7369054472aff5cf06becdfb07f6e9a5265
Fran <frankarroyop@gmail.com>
2022-03-08 08:28:19 +0100

    arreglo fallos

--- target/ApiPropia/WEB-INF/classes/data.sql
+++ target/ApiPropia/WEB-INF/classes/data.sql
@@ -3,9 +3,9 @@ insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 (2,' HOMCOM',2.5,false,28.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(3,'ZWHELL E9',1.5,true,32.3);
+(3,'ZWHELL E9',4.5,true,32.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(4,'HIBOY S2',1.0,true,25.3);
+(4,'HIBOY S2',3.0,true,25.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 (5,'Xiaomi Mini Scotter',2.5,true,13.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
@@ -15,7 +15,7 @@ insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 (8,'HIBOY X3',8.5,false,27.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(9,'Xiaomi M76',1.5,true,23.4);
+(9,'Xiaomi M76',3.5,true,23.4);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 (10,'BMW X2',2.3,true,23.1);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
@@ -27,40 +27,40 @@ insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 (14,'NOTORIUS OG',3.1,true,42.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(15,'CARLY J3',0.5,false,39.3);
+(15,'CARLY J3',2.5,false,39.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 (16,'XINYAN',6.0,true,25.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(17,'RESTMAN K34',0.3,true,29.3);
+(17,'RESTMAN K34',4.3,true,29.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 (18,'FROOZEN 4',8.1,true,32.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(19,'TWIIIN H2',1.5,false,23.3);
+(19,'TWIIIN H2',3.5,false,23.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 (20,'MOVISTAR H20',2.5,true,13.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 (21,'VODALIFE X',2.8,true,21.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(22,'REXITAR 1000',2.0,true,46.3);
+(22,'REXITAR 1000',3.0,true,46.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(23,'MOHAMED ALIN',2.1,true,42.3);
+(23,'MOHAMED ALIN',3.1,true,42.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 (24,'POEKIN S',2.2,true,32.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
 (25,'XAX2',2.5,true,13.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(26,'REBOORN S1',1.5,true,32.3);
+(26,'REBOORN S1',4.5,true,32.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(27,'OPPO ELECTRICK',0.3,true,26.3);
+(27,'OPPO ELECTRICK',2.3,true,26.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(28,'FARTY S',0.8,true,25.3);
+(28,'FARTY S',2.8,true,25.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(29,'ARRTORF2',1.5,true,42.3);
+(29,'ARRTORF2',2.5,true,42.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(30,'JORGINETE 10',0.76,true,42.3);
+(30,'JORGINETE 10',2.76,true,42.3);
 insert into patinete (idpatinete,modelo,precio_hora,disponible,kmhora) values
-(31,'JOAQUINETE 10',0.32,true,31.3);
+(31,'JOAQUINETE 10',3.32,true,31.3);
 insert into user (id,email,nickname,nombre,password,rol) values 
 (1,'aitor@magnos.com','aitor','aitor','admin1','Admin');
 insert into descuento(iddescuento,codigo,nombre_promocion,precioal_descontado,usos) values
-(1,'PRIMERO','Semana de inaguracion,',2.0,100);
+(1,'PRIMERO','Semana de inaguracion,',3.0,100);
